0da0c6a6672f0430409de62d2f9641561342c9e6
Atharva Gangras <acgangras@gmail.com>
2024-04-11 11:21:33 +0530

    add script generate one line

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ in first loop generate a container and put the divs in that.
 
 */
 const parentContainer = document.querySelector('.parentContainer')
+
+const container = document.createElement('div');
+container.classList.add('container');
+for(let i = 0; i<16;i++)
+{
+    let element = document.createElement('div');
+    element.classList.add('element');
+    container.appendChild(element);
+}
+parentContainer.appendChild(container)
